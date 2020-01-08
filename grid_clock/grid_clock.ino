55a4675a4a4fef3e65709daf06a74f3fae92fd63
Novang <qkdxorjs1002@naver.com>
2020-01-08 23:06:32 +0900

    read serial data one by one

--- grid_clock/grid_clock.ino
+++ grid_clock/grid_clock.ino
@@ -17,11 +17,11 @@ const int8_t digit_led[10][5][3] = {
         {1, 1, 1},
     },
     {
-        {0, 0, 1},
         {0, 1, 1},
         {0, 0, 1},
         {0, 0, 1},
         {0, 0, 1},
+        {0, 0, 1},
     },
     {
         {1, 1, 1},
@@ -78,13 +78,10 @@ const int8_t digit_led[10][5][3] = {
         {1, 1, 1},
         {0, 0, 1},
         {0, 0, 1},
-    }
-};
+    }};
 
 void setup()
 {
-    Serial.begin(115200);
-	ESP01.begin(115200);
     for (int8_t xdx = 0; xdx < _COL_PIXEL_SIZE; xdx++)
     {
         pinMode(pin_map_x[xdx], OUTPUT);
@@ -95,39 +92,60 @@ void setup()
         pinMode(pin_map_y[ydx], OUTPUT);
         digitalWrite(pin_map_y[ydx], HIGH);
     }
+    ESP01.begin(115200);
 }
 
-unsigned long pre_internal_time = 0;
-unsigned long external_time = 0;
 void loop()
 {
-    unsigned long time = -1;
-    if (ESP01.available())
-    {
-        time = atol(ESP01.readStringUntil('\n').c_str());
-        Serial.println(time);
-    }
-
-    if (time >= 0)
-    {
-        external_time = time;
-        pre_internal_time = (millis() / 1000);
-    }
-    matrix_set(external_time + (millis() / 1000) - pre_internal_time);
+    matrix_set(get_time());
     matrix_draw();
 }
 
-int8_t matrix_status[_ROW_PIXEL_SIZE][_COL_PIXEL_SIZE] = {{0, }, };
-void matrix_set(uint32_t _time) {
+String esp_serial_stream = "";
+unsigned long internal_time = 0;
+uint32_t online_time = 0;
+uint32_t get_time()
+{
+    if (ESP01.available())
+    {
+        char byte_from_read = ESP01.read();
+        if ((int)byte_from_read != -1)
+        {
+            esp_serial_stream += byte_from_read;
+            if (byte_from_read == '\n')
+            {
+                int32_t time = atol(esp_serial_stream.c_str());
+                if (time > 0)
+                {
+                    online_time = time;
+                    internal_time = (millis() / 1000);
+                }
+                esp_serial_stream = "";
+            }
+        }
+    }
+
+    return (online_time + (millis() / 1000) - internal_time);
+}
+
+int8_t matrix_status[_ROW_PIXEL_SIZE][_COL_PIXEL_SIZE] = {
+    {
+        0,
+    },
+};
+void matrix_set(uint32_t _time)
+{
     int8_t time[4] = {
         _time / 36000,
         (_time / 3600) % 10,
         (_time % 3600) / 600,
         (_time % 600) / 60,
     };
-    
-    for (int8_t ydx = 0; ydx < 5; ydx++) {
-        for (int8_t xdx = 0; xdx < 3; xdx++) {
+
+    for (int8_t ydx = 0; ydx < 5; ydx++)
+    {
+        for (int8_t xdx = 0; xdx < 3; xdx++)
+        {
             matrix_status[ydx][xdx] = digit_led[time[0]][ydx][xdx];
             matrix_status[ydx][xdx + 3] = digit_led[time[1]][ydx][xdx];
             matrix_status[ydx + 5][xdx] = digit_led[time[2]][ydx][xdx];
@@ -136,16 +154,21 @@ void matrix_set(uint32_t _time) {
     }
 }
 
-void matrix_draw() {
-    for (int8_t ydx = 0; ydx < _ROW_PIXEL_SIZE; ydx++) {
-        for (int8_t xdx = 0; xdx < _COL_PIXEL_SIZE; xdx++) {
-            if (matrix_status[ydx][xdx]) {
+void matrix_draw()
+{
+    for (int8_t ydx = 0; ydx < _ROW_PIXEL_SIZE; ydx++)
+    {
+        for (int8_t xdx = 0; xdx < _COL_PIXEL_SIZE; xdx++)
+        {
+            if (matrix_status[ydx][xdx])
+            {
                 digitalWrite(pin_map_x[xdx], HIGH);
             }
         }
         digitalWrite(pin_map_y[ydx], LOW);
         delay(1);
-        for (int8_t xdx = 0; xdx < _COL_PIXEL_SIZE; xdx++) {
+        for (int8_t xdx = 0; xdx < _COL_PIXEL_SIZE; xdx++)
+        {
             digitalWrite(pin_map_x[xdx], LOW);
         }
         digitalWrite(pin_map_y[ydx], HIGH);
